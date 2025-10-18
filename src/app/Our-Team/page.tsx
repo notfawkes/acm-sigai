8e56930f0f66c2f6bdcbdb0c2c2ab56595d90504
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-10-18 21:42:17 +0530

    Added navbar

--- src/app/Our-Team/page.tsx
+++ src/app/Our-Team/page.tsx
@@ -1,9 +1,45 @@
+import Link from "next/link";
+
 export default function App2() {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div className="relative h-screen w-screen">
+      <nav className="absolute top-4 right-4 z-10 flex space-x-15 px-4 py-2 rounded">
+        <Link
+          href="/"
+          className="font-family:Inter, sans-serif; text-white hover:text-blue-400 transition-colors duration-200"
+        >
+          Home
+        </Link>
+        <Link
+          href="/Events"
+          className="text-white hover:text-blue-400 transition-colors duration-200"
+        >
+          Events
+        </Link>
+        <Link
+          href="/Our-Team"
+          className="text-white hover:text-blue-400 transition-colors duration-200"
+        >
+          Team
+        </Link>
+        <Link
+          href="/Publications"
+          className="text-white hover:text-blue-400 transition-colors duration-200"
+        >
+          Publications
+        </Link>
+        <Link
+          href="/Blogs"
+          className="text-white hover:text-blue-400 transition-colors duration-200"
+        >
+          Blogs
+        </Link>
+      </nav>
+
+      {/* Iframe */}
       <iframe
         src="https://leaders-animation.vercel.app"
-        style={{ width: "100%", height: "100%", border: "none" }}
+        className="w-full h-full border-none"
         title="Leaders Animation"
       />
     </div>
